30a3b66644806a64cecc4f41bfc8d727cee6b25b
ShanzayEman <shanzayeman0@gmail.com>
2024-10-14 14:28:29 +0500

    Add files via upload

---
+++ 14oct.js
@@ -0,0 +1,8 @@
+//sign method 
+document.getElementById("demo1").innerHTML=Math.sign(1);
+document.getElementById("demo2").innerHTML=Math.sign(-1);
+document.getElementById("demo3").innerHTML=Math.sign(0);
+//power method
+document.getElementById("demo4").innerHTML=Math.pow(2,4);
+//square root method
+document.getElementById("demo5").innerHTML=Math.sqrt(36);
